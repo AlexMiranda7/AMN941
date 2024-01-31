771847fad120d08ec83bf4087c3031137dbc2257
Alexander Miranda <alex.mirandau2018@gmail.com>
2024-01-31 11:52:34 -0600

    Create temperatura.m

---
+++ temperatura.m
@@ -0,0 +1,6 @@
+display('CONVERSOR DE TEMPERATURA');
+celsius=input('Ingresa la temperatura en Celsius: ');
+kelv=celsius+ 273.15;
+fahr=(9/5)*celsius+32;
+fprintf('La temperatura en Kelvin es: %f \n', kelv);
+fprintf('La temperatura en Fareinheit es: %f \n', fahr);
