a8574be8e8cd468ac4ca1b438c49b9b659e59e57
Alexander Miranda <alex.mirandau2018@gmail.com>
2024-02-13 18:44:53 -0600

    Movimiento de carpetas y creacion de metodo de newton

---
+++ Newton/newton.m
@@ -0,0 +1,28 @@
+disp('------------METODO DE NEWTON----------');
+sysm x;
+f=input('Ingrese la funcion;  ');
+x0=input('Ingrese el punto inicial: ');
+tol=input('Ingrese el marge de error 10^-');
+tol=10^-tol;
+%Iniciamos el calculo, primero derivamos la funcion dada.
+df=diff(f);
+%Evaluamos la funcion inicial y su derivada en el punto inicial.
+fa=subs(f,x0);
+fb=sub(df,x0);
+%Formula para el valor aproximado
+x1=x0-(fa/fb);
+error=abs(x1-x0);
+cont=1;
+fprintf('n ||x0\t\t\t\t\t|| x1\t\t\t\t ||  Error\n');
+fprintf('%d || %.15f   ||  %.15f  ||  %e\n', cont,double(x0),double(x1),double(error));
+while (error>tol && cont<100)
+    cont=cont+1;
+    %Reasignamos valores
+    x0=x1;
+    fa=subs(f,x0);
+    fb=subs(df,x0);
+    x1=x0-(fa/fb);
+    error=abs(x1-x0);
+    fprintf('%d || %.15f  ||  %.15f  ||  %e\n',cont,double(x0),double(x1),double(error));
+end
+fprintf('El valor aproximado de X es: %.15f\n', double(x1));
