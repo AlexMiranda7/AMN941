b7ffb573db3a64d200b18122c327978b4d5b2279
Alexander Miranda <alex.mirandau2018@gmail.com>
2024-02-20 22:58:48 -0600

    Update clases

--- Unidad 1/Secante/secante.m
+++ Unidad 1/Secante/secante.m
@@ -12,7 +12,7 @@ x2=x1-((f1*(x1-x0))/(f1-f0));
 error=abs(x2-x1);
 cont=1;
 fprintf('\n\t|x0\t\t\t\t\t|x1\t\t\t\t\t|x2\t\t\t\t\t|error');
-fprintf('\n%d\t%.15f\t|%.15f\t|%.15f| %e',cont,double(x0),double(x1).double(x2),double(error));
+fprintf('\n%d\t|%.15f\t|%.15f\t|%.15f\t| %e',cont,double(x0),double(x1),double(x2),double(error));
 while (error>tol && cont<100)
     x0=x1;
     x1=x2;
@@ -21,6 +21,6 @@ while (error>tol && cont<100)
     f1=double(subs(f,x1));
     x2=double(x1-((f1*(x1-x0))/(f1-f0)));
     error=abs(x2-x1);
-    fprintf('\n%d\t%.15f\t|%.15f\t|%.15f| %e',cont,double(x0),double(x1).double(x2),double(error));
+    fprintf('\n%d\t|%.15f\t|%.15f\t|%.15f\t| %e',cont,double(x0),double(x1),double(x2),double(error));
 end
 fprintf('\nEl valor aproximado de x es: %.15f\n',double(x2));
